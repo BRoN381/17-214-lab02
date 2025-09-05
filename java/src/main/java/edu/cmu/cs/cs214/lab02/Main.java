53885fcc1358f1a40581b07ef968b1106496020d
BRoN381 <iamwill9028@gmail.com>
2025-09-05 10:25:54 -0400

    encapsulate java code

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,18 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        // Rectangle rectangle = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
+        // Renderer renderer = new Renderer(rectangle);
 
+        Circle circle = new Circle(5);
+        Renderer renderer = new Renderer(circle);
+        
         renderer.draw();
     }
 }
